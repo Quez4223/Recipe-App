9f659452df4a9c3b05ece5fd4aa2b4368d6a6e4c
MarQuez LeDay <quezleday@MarQuezs-MBP.attlocal.net>
2021-03-01 20:45:36 -0500

    server connected to node

---
+++ models/recipe.js
@@ -0,0 +1,11 @@
+const recipes = [
+    {
+        name: 'Pasta Aglio e Olio',
+        ingredients: 'pasta, parsley, olive oil , garlic, red pepper flakes, lemon',
+        directions: 'Salt large pot of water, bring to a boil. Cook pasta until almost done. Slice garlic cloves thinly and put aside. Heat olive oil.'
+    }
+]
+
+
+
+module.exports = recipes;
--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express');
 const APP = express();
 const PORT = 3000;
+
+
+// DATA
+const recipes = require('./models/recipe');
+
+
+// Routes
+// Index
+APP.get('/recipe', (req, res) => {
+    res.send(recipes)
+})
+
+
+
+
+
+APP.listen(PORT, () => {
+    console.log(`listening at ${PORT}`)
+})
